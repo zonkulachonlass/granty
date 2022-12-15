6bfbe42f07504c69c34bf2eb0efadb5abac65eb6
zonkulachonlass <120604183+zonkulachonlass@users.noreply.github.com>
2022-12-15 10:13:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 var randomlinks = [];
 randomlinks[0]="https://granty.ga/granty.png";
 randomlinks[1]="https://granty.ga/grant.png";
+randomlinks[1]="https://granty.ga/Adams.png";
+randomlinks[1]="https://granty.ga/Adams2.png";
+randomlinks[1]="https://granty.ga/Adams3.png";
+randomlinks[1]="https://granty.ga/Adams4.png";
+randomlinks[1]="https://granty.ga/Adams5.png";
+randomlinks[1]="https://granty.ga/Adams6.png";
+randomlinks[1]="https://granty.ga/Adams7.png";
+randomlinks[1]="https://granty.ga/Adams8.png";
+randomlinks[1]="https://granty.ga/Adams9.png";
+randomlinks[1]="https://granty.ga/Adams10.png";
+randomlinks[1]="https://granty.ga/Adams11.png";
+randomlinks[1]="https://granty.ga/Adams12.png";
+randomlinks[1]="https://granty.ga/Adams13.png";
+randomlinks[1]="https://granty.ga/Adams14.png";
+randomlinks[1]="https://granty.ga/Adams15.png";
+randomlinks[1]="https://granty.ga/Adams16.png";
 
 function randomlink(){
   window.open(randomlinks[Math.floor(Math.random()*randomlinks.length)]);
