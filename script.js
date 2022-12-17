e3e0c045052ff40ef140ac07b4b33688eaf11ad4
zonkulachonlass <120604183+zonkulachonlass@users.noreply.github.com>
2022-12-16 17:58:33 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 var randomlinks = [];
-randomlinks[0]="https://grantiest.ga/granty.png";
-randomlinks[1]="https://grantiest.ga/grant.png";
-randomlinks[2]="https://grantiest.ga/Adams.png";
-randomlinks[3]="https://grantiest.ga/Adams2.png";
-randomlinks[4]="https://grantiest.ga/Adams3.png";
-randomlinks[5]="https://grantiest.ga/Adams4.png";
-randomlinks[6]="https://grantiest.ga/Adams5.png";
-randomlinks[7]="https://grantiest.ga/Adams6.png";
-randomlinks[8]="https://grantiest.ga/Adams7.png";
-randomlinks[9]="https://grantiest.ga/Adams8.png";
-randomlinks[10]="https://grantiest.ga/Adams9.png";
-randomlinks[11]="https://grantiest.ga/Adams10.png";
-randomlinks[12]="https://grantiest.ga/Adams11.png";
-randomlinks[13]="https://grantiest.ga/Adams12.png";
-randomlinks[14]="https://grantiest.ga/Adams13.png";
-randomlinks[15]="https://grantiest.ga/Adams14.png";
-randomlinks[16]="https://grantiest.ga/Adams15.png";
-randomlinks[17]="https://grantiest.ga/Adams16.png";
-randomlinks[18]="https://grantiest.ga/Adams17.png";
-randomlinks[19]="https://grantiest.ga/Adams18.png";
-randomlinks[20]="https://grantiest.ga/Adams19.png";
-randomlinks[21]="https://grantiest.ga/Adams20.png";
-randomlinks[22]="https://grantcadams.com/";
-randomlinks[23]="https://grantcadams.com/drinking-with-grant";
-randomlinks[24]="https://grantcadams.com/bio-1";
-randomlinks[25]="https://grantiest.ga/Adams21.jpg";
-randomlinks[26]="https://grantiest.ga/Adams22.jpg";
-randomlinks[27]="https://grantiest.ga/Adams23.png.webp";
+randomlinks[0] = "https://grantiest.ga/granty.png";
+randomlinks[1] = "https://grantiest.ga/grant.png";
+randomlinks[2] = "https://grantiest.ga/Adams.png";
+randomlinks[3] = "https://grantiest.ga/Adams2.png";
+randomlinks[4] = "https://grantiest.ga/Adams3.png";
+randomlinks[5] = "https://grantiest.ga/Adams4.png";
+randomlinks[6] = "https://grantiest.ga/Adams5.png";
+randomlinks[7] = "https://grantiest.ga/Adams6.png";
+randomlinks[8] = "https://grantiest.ga/Adams7.png";
+randomlinks[9] = "https://grantiest.ga/Adams8.png";
+randomlinks[10] = "https://grantiest.ga/Adams9.png";
+randomlinks[11] = "https://grantiest.ga/Adams10.png";
+randomlinks[12] = "https://grantiest.ga/Adams11.png";
+randomlinks[13] = "https://grantiest.ga/Adams12.png";
+randomlinks[14] = "https://grantiest.ga/Adams13.png";
+randomlinks[15] = "https://grantiest.ga/Adams14.png";
+randomlinks[16] = "https://grantiest.ga/Adams15.png";
+randomlinks[17] = "https://grantiest.ga/Adams16.png";
+randomlinks[18] = "https://grantiest.ga/Adams17.png";
+randomlinks[19] = "https://grantiest.ga/Adams18.png";
+randomlinks[20] = "https://grantiest.ga/Adams19.png";
+randomlinks[21] = "https://grantiest.ga/Adams20.png";
+randomlinks[22] = "https://grantcadams.com/";
+randomlinks[23] = "https://grantcadams.com/drinking-with-grant";
+randomlinks[24] = "https://grantcadams.com/bio-1";
+randomlinks[25] = "https://grantiest.ga/Adams21.jpg";
+randomlinks[26] = "https://grantiest.ga/Adams22.jpg";
+randomlinks[27] = "https://grantiest.ga/Adams23.png.webp";
 
-function randomlink(){
-  window.open(randomlinks[Math.floor(Math.random()*randomlinks.length)]);
+function randomlink() {
+  window.open(randomlinks[Math.floor(Math.random() * randomlinks.length)]);
+}
+
+function link() {
+  window.open(randomlinks[27])
 }
